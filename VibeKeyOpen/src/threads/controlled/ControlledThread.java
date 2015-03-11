7bd21c15cc180ed523a945208409ac1026730c3c
Rose Reatherford <rosereatherford@yahoo.com>
2015-03-11 13:13:06 -0400

    Minor Change.

--- VibeKeyOpen/src/threads/controlled/ControlledThread.java
+++ VibeKeyOpen/src/threads/controlled/ControlledThread.java
@@ -3,10 +3,8 @@ package threads.controlled;
 import primary_manager.VibeKey;
 
 public class ControlledThread extends Thread {
-	private ControlledRunner runner;
 	public void run(ControlledRunner runner) {
 		System.out.println("I am thread: " + this.getId());
-		this.runner = runner;
 		runner.execute();
 		VibeKey.manager.returnThread(this);
 	}
