5c0e9efcb4d276849e2f69bd2949f71cf68b2bb0
Rose Reatherford <rosereatherford@yahoo.com>
2015-03-16 13:59:36 -0400

    Changed some test logic.

--- VibeKeyOpen/Tests/runnables/test/BufferTest.java
+++ VibeKeyOpen/Tests/runnables/test/BufferTest.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import primary_manager.PrimaryManager;
+import core_objects_abstract.Song;
+
 public class BufferTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Song song = new Song("C:\\Users\\reatherl\\git\\VibeKeyOpen\\VibeKeyOpen\\VibeKeyOpen\\Songs", 201, 
+				"Lose Myself", 3219456);
+		
+		PrimaryManager manager = new PrimaryManager();
+		manager.bufferSong(song);
 	}
 
 }
