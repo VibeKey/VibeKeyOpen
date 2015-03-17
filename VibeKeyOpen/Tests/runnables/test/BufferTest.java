490afe28792558019f30b6306ec46b2f6d7bd18a
Rose Reatherford <rosereatherford@yahoo.com>
2015-03-17 14:21:40 -0400

    Adding more stress tests.

--- VibeKeyOpen/Tests/runnables/test/BufferTest.java
+++ VibeKeyOpen/Tests/runnables/test/BufferTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 
 import org.junit.Test;
 
@@ -14,7 +13,7 @@ import core_objects_abstract.Song;
 public class BufferTest {
 
 	@Test
-	public void testOneSong() throws InterruptedException, IOException {
+	public void testOneSong() throws Exception {
 		
 		File songFile = new File("Songs/LoseMyself.mp3");
 		
@@ -48,6 +47,37 @@ public class BufferTest {
 		}
 		
 		in.close();
+	}
+	
+	@Test
+	public void testTwoSongs() throws InterruptedException {
+		File songFile1 = new File("Songs/LoseMyself.mp3");
+		File songFile2 = new File("Songs/Rift.mp3");
+		
+		Song song1 = new Song(songFile1.getAbsolutePath(), 201, 
+				"Lose Myself", (int) songFile1.length());
+		Song song2 = new Song(songFile2.getAbsolutePath(), 201, 
+				"Rift", (int) songFile2.length());
+		
+		VibeKey.manager.bufferSong(song1);
+		VibeKey.manager.bufferSong(song2);
+		
+		while(!song1.finishedBuffering() && !song2.finishedBuffering()){
+			synchronized(song1) {
+				song1.wait();
+			}
+			
+			synchronized(song2) {
+				song2.wait();
+			}
+		}
+		
+		long size = (long) Math.ceil(3217030 / Song.BUFFER_SIZE);
+		
+		assertTrue("The estimated size of song 1 is not the actual size.\nActual Size: " 
+				+ song1.getBuffer().size() + "\nEstimated Size: " + size, song1.getBuffer().size() == size);
+		assertTrue("The estimated size of song 2 is not the actual size.\nActual Size: " 
+				+ song2.getBuffer().size() + "\nEstimated Size: " + size, song2.getBuffer().size() == size);
 	}
 
 }
