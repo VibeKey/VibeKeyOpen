defd6ae22b0626963bff43edd62aa7e981c34b1a
Jonathan Jungck <jonathan@jungck.com>
2015-03-22 15:11:45 -0400

    SQL Query
    Trent and Jonathan

---
+++ VibeKeyOpen/src/mysql/Query.java
@@ -0,0 +1,25 @@
+package mysql;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Query {
+	protected String query;
+	protected Connection conn;
+
+	public Query(String query, Connection conn) {
+		this.query = query;
+		this.conn = conn;
+	}
+	
+	public void execute() throws SQLException {
+		java.sql.Statement stmt = conn.createStatement();
+		ResultSet results = stmt.executeQuery(query);
+		while (results.next()) {
+			int id = results.getInt("id");
+			String name = results.getString("name");
+			System.out.println("ID: " + id + ", NAME: " + name);
+		}
+	}
+}
