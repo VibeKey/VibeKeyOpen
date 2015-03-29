7540e5989c345095fe1a3ff20f30a705316eb348
Benedict Wong <bennydictwong@gmail.com>
2015-03-29 16:33:18 -0400

    Added index and request handler

---
+++ VibeKeyOpen/src/web/RequestHandler.java
@@ -0,0 +1,12 @@
+package web;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestHandler {
+
+	public static String handleGetSongDetails(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
