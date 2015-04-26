69d722a2353a38b7e52769b23ae2483b121c7e6e
Rose Reatherford <rosereatherford@yahoo.com>
2015-04-25 21:42:25 -0400

    MORE WEB STUFF GOD

---
+++ WebContent/hover.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+		$('.tabLinkContainer').hover(function(){
+			$(this).addClass('tabLinkContainerHover');
+		}, function() {
+			$(this).removeClass('tabLinkContainerHover');
+		});
+	
+		$('#search').sidr({
+			name: 'searchFunc',
+			source: '#searchTab',
+			onOpen: function(){
+				document.getElementById("searchSpan").style.width = "75%";
+				
+			},
+			onClose: function(){
+				document.getElementById("searchSpan").style.width = "";
+			}
+		});
+	});
--- WebContent/tabScript.js
+++ WebContent/tabScript.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
-		$('#search').sidr({
-		name: 'searchFunc',
-		source:'#searchTab',
-		side: 'left' 
-		});
-		$('#news').sidr({
-		name: 'newsFunc',
-		source:'#newsTab',
-		side: 'left' 
-		});
-	});
+window.onload = function () {
+	var height = ((window.height - 100) / (7 * 2)) + "px",
+		heightHalf = ((window.height - 100) / (7)) + "px",
+		container = document.getElementById("container");
+ 
+	container.style.height = height;
+	container.style['line-height'] = height;
+		
+	var arrows = document.querySelectorAll(".arrow-right");
+ 
+	for (var i = 0; i < arrows.length; i++) {
+	}
+};
+	
+	
