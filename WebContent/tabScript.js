17641b762d990cba1e7450dd1d2f08c878c36f51
Rose Reatherford <rosereatherford@yahoo.com>
2015-04-28 14:23:19 -0400

    More web.

--- WebContent/tabScript.js
+++ WebContent/tabScript.js
@@ -1,15 +1,16 @@
-window.onload = function () {
+$(document).ready(function() {
+	
 	var height = ((window.height - 100) / (7 * 2)) + "px",
-		heightHalf = ((window.height - 100) / (7)) + "px",
-		container = document.getElementById("container");
+		heightHalf = ((window.height - 100) / (7)) + "px";
+	var container = $('.tabLinkContainer');
  
-	container.style.height = height;
-	container.style['line-height'] = height;
+	container.css("height", height);
+	container.css('line-height', height);
 		
 	var arrows = document.querySelectorAll(".arrow-right");
  
 	for (var i = 0; i < arrows.length; i++) {
 	}
-};
+});
 	
 	
