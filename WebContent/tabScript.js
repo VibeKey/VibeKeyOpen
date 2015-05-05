a51e438ec5b95b690c403801bde87c385fa25421
Rose Reatherford <rosereatherford@yahoo.com>
2015-05-05 13:47:16 -0400

    Animations.

--- WebContent/tabScript.js
+++ WebContent/tabScript.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 		var heightHalf = height/2;
 		var container = $(".tabLinkContainer");
 	 
-		$(".tabLinkContainer").css('height',height);
+		$(".tabLinkContainer").css('height',height + 'px');
+		$(".tabLinkContainer").css('line-height',height + 'px');
 		//$(".tabLinkContainer").css('line-height',heightHalf);
 			
 		var arrows = document.querySelectorAll(".arrow-right");
@@ -20,42 +21,6 @@ $(document).ready(function() {
 		$(".arrow-right").css('border-left',.33*height);
 		$(".arrow-right").css('border-left-style',"solid");
 		$(".arrow-right").css('border-left-color',"rgb(147,147,147)");
-	
-		var createSong = function(awesomeSong,awesomeArtist,likes){
-			var songItem = $('<div></div');
-			songItem.attr("class","songItem");
-			songItem.attr("draggable","true");
-			var votingPart = $('<span></span>');
-			votingPart.attr("class","votingPart");
-			var basicPart = $('<span></span>');
-			basicPart.attr("class","basicPart");
-			
-			var up = $('<div></div>');
-			up.attr("class","songUpArrow");
-			var down = $('<div></div>');
-			down.attr("class","songDownArrow");
-			var likeNumber = $('<div>' + likes + '</div>');
-			likeNumber.attr("class","likeNumber ");
-			
-			var songName = $('<div>' + awesomeSong + '</div>');
-			songName.attr("class","songName");
-			var bandName = $('<div>' + awesomeArtist + '</div>');
-			bandName.attr("class","bandName");
-			
-			$(".playlist").append(songItem);
-			
-			songItem.append(votingPart);
-			songItem.append(basicPart);
-			
-			votingPart.append(up);
-			votingPart.append(likeNumber);
-			votingPart.append(down);
-			
-			basicPart.append(songName);
-			basicPart.append(bandName);
-		}
-			createSong("Shake It Off", "Taylor Swift", "9000+");
-			createSong("Roses", "Black Eyed Peas", "3000");
 	
 	}
 	
