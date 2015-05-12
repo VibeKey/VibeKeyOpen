c1cdbb316f44ff38c8b9669111c710e22813f86c
Rose Reatherford <rosereatherford@yahoo.com>
2015-05-12 15:55:10 -0400

    Streaming now works, animations also pause and play with audio.

--- WebContent/streamScript.js
+++ WebContent/streamScript.js
@@ -1,28 +1,78 @@
-var isPlaying = false;
+$(document).ready(function() {	
+	document.getElementById("audioStream").addEventListener('error', function(e){
+		failed(e);
+	}, true);
+});
+
+var isPlaying = true;
+var firstPlay = true;
 	
 function pressPlayerButton(){
 	var player = document.getElementById("player");
+	var audio = document.getElementById("audioStream");
 	if (!isPlaying){
-		$(document.body).append("<audio id='audioStream' autoplay preload=none source src='http://wmhd-test.csse.rose-hulman.edu:8000/radio' ></audio>");
+		// Resumes audio.
+		audio.play();
 		isPlaying = true;
 		
+		// Resumes animations.
+		for (i=1; i <= 5; i++) {
+			var name = "line" + i;
+			document.getElementById(name).style["-webkit-animation-play-state"] = "running";
+			document.getElementById(name).style["animation-play-state"] = "running";
+		}
+		
+		// Creates the pause image.
 		var pauseButton = document.createElement('img');
 		pauseButton.src = "http://wmhd2.rose-hulman.edu/pausebutton.png";
 		pauseButton.className = "playerButton";
 		pauseButton.id = "pauseButton";
 		
+		// Removes the play button and adds the pause button.
 		player.removeChild(document.getElementById("playButton"));
 		player.appendChild(pauseButton);
 	} else {
-		$("#audioStream").remove();
+		// Pauses audio.
+		audio.pause();
 		isPlaying = false;
 		
+		// Pauses Animations
+		for (i=1; i <= 5; i++) {
+			var name = "line" + i;
+			document.getElementById(name).style["-webkit-animation-play-state"] = "paused";
+			document.getElementById(name).style["animation-play-state"] = "paused";
+		}
+		
+		// Creates the play image.
 		var playButton = document.createElement('img');
 		playButton.src = "http://wmhd2.rose-hulman.edu/playbutton.png";
 		playButton.className = "playerButton";
 		playButton.id = "playButton";
 		
+		// Removes the pause button and adds the play button.
 		player.removeChild(document.getElementById("pauseButton"));
 		player.appendChild(playButton);
 	}
+}
+
+function failed(e) {
+	// audio playback failed - show a message saying why
+	// to get the source of the audio element use $(this).src
+	switch (e.target.error.code) {
+		case e.target.error.MEDIA_ERR_ABORTED:
+			alert('You aborted the video playback.');
+			break;
+		case e.target.error.MEDIA_ERR_NETWORK:
+			alert('A network error caused the audio download to fail.');
+			break;
+		case e.target.error.MEDIA_ERR_DECODE:
+			alert('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+			break;
+		case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+			alert('The video audio not be loaded, either because the server or network failed or because the format is not supported.');
+			break;
+		default:
+			alert('An unknown error occurred.');
+			break;
+		}
 }
