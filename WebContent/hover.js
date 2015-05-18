195d29561d353034ed333bfb0b2a70bd4e741b8e
Rose Reatherford <rosereatherford@yahoo.com>
2015-05-17 20:31:35 -0400

    Big web update.

--- WebContent/hover.js
+++ WebContent/hover.js
@@ -1,104 +1,78 @@
+var first = true;
+
+function makeTab(tabName) {
+	buildTab(tabName);
+	
+	var span = document.getElementById(tabName + "Span");
+	var arrow = document.getElementById(tabName + "Arrow");
+	
+	$('#' + tabName).sidr({
+		name: tabName + 'Func',
+		source: '#' + tabName + 'Tab',
+		renaming: false,
+		onOpen: function(){
+			span.style["width"] = "75%";
+			span.style["background-color"] = "rgb(153, 000, 000)";
+			arrow.style["border-left-color"] = "rgb(153, 000, 000)";
+			
+			if (first) {
+				document.dispatchEvent(new UIEvent("resize",
+					{ bubbles: true, cancelable: false }));
+				first = false;
+			}
+		},
+		onClose: function(){
+			span.style["width"] = null;
+			span.style["background-color"] = null;
+			arrow.style["border-left-color"] = null;
+		}
+	});
+}
+
+function buildTab(tabName) {
+	var a = document.createElement('a');
+	a.id = tabName;
+	a.href = "#" + tabName + "Func";
+	
+	var contain = document.createElement('div');
+	contain.className = "tabLinkContainer";
+	a.appendChild(contain);
+	
+	var imgContain = document.createElement('div');
+	imgContain.className = "tabLink";
+	imgContain.id = tabName + "Span";
+	contain.appendChild(imgContain);
+	
+	var img = document.createElement('img');
+	img.className = "tabLinkImg";
+	img.src = "/images/" + tabName + ".png";
+	imgContain.appendChild(img);
+	
+	var arrow = document.createElement('div');
+	arrow.className = "arrow-right";
+	arrow.id = tabName + "Arrow";
+	contain.appendChild(arrow);
+	
+	document.getElementById("container").appendChild(a);
+}
+
 $(document).ready(function() {
+		//makes the search tab pull out
+		var tabs = ["search", "news", "soundboard", "calendar",
+						"djChat", "audience", "settings"]
+		for (i = 0, len = tabs.length; i < len; i++) {
+			makeTab(tabs[i]);
+		}
+		
 		$('.tabLinkContainer').hover(function(){
 			$(this).addClass('tabLinkContainerHover');
 		}, function() {
 			$(this).removeClass('tabLinkContainerHover');
 		});
-		//makes the search tab pull out
-		$('#search').sidr({
-			name: 'searchFunc',
-			source: '#searchTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("searchSpan").style.width = "75%";
-				trig();
-			},
-			onClose: function(){
-				document.getElementById("searchSpan").style.width = "";
-			}
-		});
-		//makes the news tab pull out
-		$('#news').sidr({
-			name: 'newsFunc',
-			source: '#newsTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("newsSpan").style.width = "75%";
-				
-			},
-			onClose: function(){
-				document.getElementById("newsSpan").style.width = "";
-			}
-		});
-		//makes the soundboard tab pull out
-		$('#soundboard').sidr({
-			name: 'soundboardFunc',
-			source: '#soundboardTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("soundboardSpan").style.width = "75%";
-				
-			},
-			onClose: function(){
-				document.getElementById("soundboardSpan").style.width = "";
-			}
-		});
-		//makes the calendar tab pull out
-		$('#calendar').sidr({
-			name: 'calendarFunc',
-			source: '#calendarTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("calendarSpan").style.width = "75%";
-				
-			},
-			onClose: function(){
-				document.getElementById("calendarSpan").style.width = "";
-			}
-		});
-		//makes the djChat tab pull out
-		$('#djChat').sidr({
-			name: 'djChatFunc',
-			source: '#djChatTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("djChatSpan").style.width = "75%";
-				
-			},
-			onClose: function(){
-				document.getElementById("djChatSpan").style.width = "";
-			}
-		});
-		//makes the audience tab pull out
-		$('#audience').sidr({
-			name: 'audienceFunc',
-			source: '#audienceTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("audienceSpan").style.width = "75%";
-				
-			},
-			onClose: function(){
-				document.getElementById("audienceSpan").style.width = "";
-			}
-		});
-		//makes the settings tab pull out
-		$('#settings').sidr({
-			name: 'settingsFunc',
-			source: '#settingsTab',
-			renaming: false,
-			onOpen: function(){
-				document.getElementById("settingsSpan").style.width = "75%";
-				
-			},
-			onClose: function(){
-				document.getElementById("settingsSpan").style.width = "";
-			}
-		});
-		
-		
 	});
 	
+	
+	
 function trig() {
-	$(window).triggerHandler("resize");
+	
 }
--- WebContent/tabScript.js
+++ WebContent/tabScript.js
@@ -11,18 +11,14 @@ function changeTabs(){
 	 
 		$(".tabLinkContainer").css('height',height + 'px');
 		$(".tabLinkContainer").css('line-height',height + 'px');
+		$(".tabLinkImg").css('height',height + 'px');
+		$(".tabLinkImg").css('width',height + 'px');
 		//$(".tabLinkContainer").css('line-height',heightHalf);
 			
 		var arrows = document.querySelectorAll(".arrow-right");
 	 
 		$(".arrow-right").css('border-top-width',.5*height);
-		$(".arrow-right").css('border-top-style',"solid");
-		$(".arrow-right").css('border-top-color',"transparent");
-		$(".arrow-right").css('border-bottom',.52*height);
-		$(".arrow-right").css('border-bottom-style',"solid");
-		$(".arrow-right").css('border-bottom-color',"transparent");
-		$(".arrow-right").css('border-left',.33*height);
-		$(".arrow-right").css('border-left-style',"solid");
-		$(".arrow-right").css('border-left-color',"rgb(147,147,147)");
+		$(".arrow-right").css('border-bottom-width',.52*height);
+		$(".arrow-right").css('border-left-width',.33*height);
 	}
 	
