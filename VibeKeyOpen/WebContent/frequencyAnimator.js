dbb5726e462df4f91410d226798ae61f097a10c3
Rose Reatherford <rosereatherford@yahoo.com>
2015-05-19 19:01:35 -0400

    Fixed aniamtions.

--- VibeKeyOpen/WebContent/frequencyAnimator.js
+++ VibeKeyOpen/WebContent/frequencyAnimator.js
@@ -21,39 +21,46 @@ function initAudio(audio) {
 	var context = new AudioContext();
 	var analyser = context.createAnalyser(); 
 	analyser.smoothingTimeConstant = 0.85;
+	analyser.minDecibels = -90;
+	analyser.maxDecibels = -10;
 	
 	var source = context.createMediaElementSource(audio); 
 	source.connect(analyser);
 	analyser.connect(context.destination);
 	
-	analyser.fftSize = 32;
+	analyser.fftSize = 64;
 	var bufferLength = analyser.frequencyBinCount;
 	var dataArray = new Uint8Array(bufferLength);
+	var canvas = document.querySelector('.bounce');
+	var canvasCtx = canvas.getContext("2d");
 	
-	var visualisation = document.getElementById("bounceAnimation");
-	var barSpacingPercent = 100 / bufferLength;
-    for (var i = 0; i < bufferLength; i++) {
-		var div = document.createElement('div');
-		div.style["left"] = i * barSpacingPercent + "%";
-		div.style["width"] = barSpacingPercent + "%";
-		div.style["height"] = "5%";
-		div.className = "lineDance";
-		
-    	visualisation.appendChild(div);
-    }
-    var bars = visualisation.children;
+	var intendedWidth = document.querySelector('.playhead').clientWidth;
+	canvas.setAttribute('width',intendedWidth);
 	
-	function draw(){
-		requestAnimationFrame(draw);
-		analyser.getByteFrequencyData(dataArray);
-		
-		for (i = 0; i < bars.length; i++) {
-			bars[i].style["height"] = (dataArray[i] * -1) + '%';
-			if(dataArray[i] != 0) {
-                alert(dataArray[i]);
-            }
+	canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	function draw() {
+		  drawVisual = requestAnimationFrame(draw);
+
+		  analyser.getByteFrequencyData(dataArray);
+
+		  canvasCtx.fillStyle = 'white';
+		  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+		  var barWidth = (canvas.width / bufferLength) * 2.5;
+		  var barHeight;
+		  var x = 0;
+
+		  for(var i = 0; i < bufferLength; i++) {
+			barHeight = dataArray[i];
+
+			canvasCtx.fillStyle = 'rgba(153, 000, 000, 0.05)';
+			canvasCtx.fillRect(x,canvas.height-barHeight/2,
+				barWidth,barHeight/2);
+
+			x += barWidth + 1;
+		  }
 		};
-	}
 	
 	draw();
 }
