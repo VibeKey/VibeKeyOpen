91df517999208747bed330d1b0432aef346d6610
CHENG ZENG <zengc@rose-hulman.edu>
2015-10-19 18:38:03 -0400

    add schedule playlist support on website

--- VibeKeyWeb.new/js/scheduleMusic.js
+++ VibeKeyWeb.new/js/scheduleMusic.js
@@ -1,11 +1,12 @@
 var fireRef = new Firebase("https://vibekey-open.firebaseio.com/");
 var genreListRef = fireRef.child("songs").child("genreList");
 var songListRef = fireRef.child("songs").child("allSongs");
+var playlistListRef = fireRef.child("playlists");
 var controls = fireRef.child("controls");
 
 //populate genre drop down in schedule genre
 genreListRef.on("value", function(snapshot) {
-  var genresDropdown = document.getElementById("genresList");
+  //var genresDropdown = document.getElementById("genresList");
   snapshot.forEach(function(data) {
     var genre = data.val();
     var option = document.createElement("option");
@@ -17,13 +18,27 @@ genreListRef.on("value", function(snapshot) {
 
 //populate song drop down in schedule song
 songListRef.once("value", function(snapshot) {
-  var genresDropdown = document.getElementById("songsList");
+  //var playsDropdown = document.getElementById("playsList");
   snapshot.forEach(function(childSnapshot) {
     var songs = childSnapshot.val();
     var songName = songs.title;
+    var songPath = songs.path;
     var option = document.createElement("option");
-    option.text = option.value = songName;
+    option.text = songName;
+    option.value = songPath;
     $('.songsList').append(option);
+  });
+});
+
+//populate playlist drop down in schedule song
+playlistListRef.once("value", function(snapshot) {
+  //var songsDropdown = document.getElementById("songsList");
+  snapshot.forEach(function(childSnapshot) {
+    var lists = childSnapshot.val();
+    var listName = lists.name;
+    var option = document.createElement("option");
+    option.text = option.value = listName;
+    $('.playsList').append(option);
   });
 });
 
@@ -38,7 +53,14 @@ function scheduleGenre(list){
 }
 
 function scheduleSong(list){
-  var selectedSong = list.value;
-  var command = createCommand(false, "setSong", {"song" : selectedSong});
+  var selectedSongName = list.options[list.selectedIndex].text;
+  var selectedSongPath = list.value;
+  var command = createCommand(false, "addToFrontOfQueue", {"songPath" : selectedSongPath, "song" : selectedSongName});
+  controls.set(command);
+}
+
+function scheduleList(list) {
+  var selectedList = list.options[list.selectedIndex].text;
+  var command = createCommand(false, "setPlaylist", {"playlist" : selectedList});
   controls.set(command);
 }
