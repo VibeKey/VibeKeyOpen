472ccc4c538786dce3f974486f060e44ba95a181
CHENG ZENG <zengc@rose-hulman.edu>
2015-10-27 20:56:49 -0400

    add news item and display

--- VibeKeyWeb.new/js/addNews.js
+++ VibeKeyWeb.new/js/addNews.js
@@ -1,8 +1,9 @@
 function addNews() {
-	// var fireRef = new Firebase("https://vibekey-open.firebaseio.com/");
-	// var newsRef = fireRef.child("news");
-	var title = document.getElementById("newsTitle");
-	console.log(title.value);
-	//var newsItem = requestSongsRef.push();
-	//newsItem.set();
+	var title = document.getElementById("newsTitle").value;
+	var description = document.getElementById("newsDescription").value;
+	var date = document.getElementById("newsDate").value;
+	var fireRef = new Firebase("https://vibekey-open.firebaseio.com/");
+	var newsRef = fireRef.child("news");
+	var newsItem = newsRef.push();
+	newsItem.set({'title' : title, 'description' : description, "date" : date});
 }
--- VibeKeyWeb.new/js/requestSong.js
+++ VibeKeyWeb.new/js/requestSong.js
@@ -1,7 +1,6 @@
 function requestSong(request) {
 		var fireRef = new Firebase("https://vibekey-open.firebaseio.com/");
 		var requestSongsRef = fireRef.child("requestedSongs");
-
 		var requestSong = requestSongsRef.push();
 		requestSong.set(request.title);
 }
