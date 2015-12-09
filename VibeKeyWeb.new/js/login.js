c3f6c90463342f4e84eff8868deb45ab662d74ff
CHENG ZENG <zengc@rose-hulman.edu>
2015-12-08 21:56:30 -0500

    update login, separate views for different users

--- VibeKeyWeb.new/js/login.js
+++ VibeKeyWeb.new/js/login.js
@@ -37,3 +37,17 @@ function changeToUserView() {
 	$(".DjView").hide();
 	$(".UserView").show();
 }
+
+// function register() {
+// 	var ref = new Firebase("https://vibekey-open.firebaseio.com/");
+// 	ref.createUser({
+//   		email    : "bobtony@firebase.com",
+// 	  	password : "correcthorsebatterystaple"
+// 	}, function(error, userData) {
+// 	  if (error) {
+// 	    console.log("Error creating user:", error);
+// 	  } else {
+// 	    console.log("Successfully created user account with uid:", userData.uid);
+// 	  }
+// 	});
+// }
--- VibeKeyWeb.new/js/showLoginForm.js
+++ VibeKeyWeb.new/js/showLoginForm.js
@@ -6,26 +6,18 @@ $("#modal_trigger").leanModal({
 });
 
 $(function() {
-        // Calling Login Form
-        $("#login_form").click(function() {
-                $(".social_login").hide();
-                $(".user_login").show();
-                return false;
-        });
-
         // Calling Register Form
         $("#register_form").click(function() {
-                $(".social_login").hide();
+                $(".user_login").hide();
                 $(".user_register").show();
                 $(".header_title").text('Register');
                 return false;
         });
 
-        // Going back to Social Forms
+        // Going back to Login Form
         $(".back_btn").click(function() {
-                $(".user_login").hide();
                 $(".user_register").hide();
-                $(".social_login").show();
+                $(".user_login").show();
                 $(".header_title").text('Login');
                 return false;
         });
