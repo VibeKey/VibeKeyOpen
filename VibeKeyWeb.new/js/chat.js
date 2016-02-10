4a928eb816c61cf86e3b0881e735e682d4e4a545
CHENG ZENG <zengc@rose-hulman.edu>
2016-02-10 14:48:28 -0500

    continue chat feature
    works for individual user
    need to sync message among users from different laptop

--- VibeKeyWeb.new/js/chat.js
+++ VibeKeyWeb.new/js/chat.js
@@ -6,14 +6,32 @@ function chat() {
     if (authData) {
       var username = authData.password.email;
       var messageField = document.getElementsByClassName('messageInput')[1];
-      var messageList = document.getElementsByClassName('chat_messages')[1];
       var message = messageField.value;
+      
+      //ADD TO FIREBASE DATA
       messagesRef.push({name:username, text:message});
 
-      messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-        //GET DATA
+      refreshChat();
+
+      //CLEAR TEXTBOX
+      messageField.value = "";
+    } else {
+      alert("You must login to chat.")
+    }
+  });  
+}
+
+function refreshChat() {
+  var fireRef = new Firebase("https://vibekey-open.firebaseio.com/");
+  var messagesRef = fireRef.child("chatMessage");
+  var messageList = document.getElementsByClassName('chat_messages')[1];
+
+  messageList.innerHTML = "";
+  messagesRef.on('value', function (data) {
+    data.forEach(function (snapshot) {
+      //GET DATA
         var data = snapshot.val();
-        var username = data.name;
+        var username = data.name.split("@")[0];
         var message = data.text;
 
         //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
@@ -27,12 +45,6 @@ function chat() {
         option.id = option.value = "message";
         option.text =  username + " : " + message;
         messageList.appendChild(option);
-
-        //CLEAR TEXTBOX
-        messageField.value = "";
-      });
-    } else {
-      alert("You must login to chat.")
-    }
-  });  
+    });
+  });
 }
