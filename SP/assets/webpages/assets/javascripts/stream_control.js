004392934a5d525fd57ed23e801f80403e3e0fb3
CHENG ZENG <zengc@rose-hulman.edu>
2016-04-24 13:02:00 -0400

    stream control update

--- SP/assets/webpages/assets/javascripts/stream_control.js
+++ SP/assets/webpages/assets/javascripts/stream_control.js
@@ -3,3 +3,87 @@ function nextSong() {
   var command = createCommand(true, "nextSong", {});
   controls.push(command);
 }
+
+function search_for_stream() {
+	var searchWord = document.getElementById('search_key_stream').value;
+	var allsongsRef = new Firebase(FIREBASE_REF).child("songs").child("allSongs");
+	var search_result = document.getElementById('search_result_stream');
+	search_result.innerHTML = "";
+	allsongsRef.on("value", function(snapshot) {
+		var main = snapshot.val();
+		snapshot.forEach(function(childSnapshot) {
+			var id = childSnapshot.key();
+		 	var childData = childSnapshot.val();
+		 	var songAlbum = childData.album;
+		  	var songName = childData.title;
+		  	var songBand = childData.artist;
+		  	var songGenre = childData.genre;
+		  	var songPath = childData.path;
+		  	var votes = childData.netVotes;
+		  	if(songAlbum.indexOf(searchWord) > -1 || songName.indexOf(searchWord) > -1 || songBand.indexOf(searchWord) > -1 || songGenre.indexOf(searchWord) > -1) {
+		  		var option = document.createElement('option');
+		  		option.id = songPath;
+		  		option.text = songName + ' by ' + songBand;
+		  		search_result.appendChild(option);
+		  	}
+		});
+	}, function (errorObject) {
+		console.log("The read failed: " + errorObject.code);
+	});
+}
+function playSong(){
+  var allSongsList = document.getElementById("allSongsList");
+  var selectedSong = allSongsList.value;
+  var controls = new Firebase(FIREBASE_REF).child("controls/command");
+  var command = createCommand(true, "setGenre", {"genre" : selectedSong});
+  controls.push(command);
+}
+
+function populateGenreList() {
+  var genresList = document.getElementById("genresList");
+  var genresListRef = new Firebase(FIREBASE_REF).child("songs/genreList");
+  genresListRef.on("value", function(snapshot) {
+    genresList.innerHTML = "";
+      snapshot.forEach(function(data) {
+          var id = data.key();
+          var genre = data.val();
+          var option = document.createElement('option');
+          option.id = option.value = id;
+          option.text =  genre;
+          genresList.appendChild(option);
+      });
+  });
+}
+
+function playGenre(){
+  var genresList = document.getElementById("genresList");
+  var selectedGenre = genresList.value;
+  var controls = new Firebase(FIREBASE_REF).child("controls/command");
+  var command = createCommand(true, "setGenre", {"genre" : selectedGenre});
+  controls.push(command);
+}
+
+function populatePlaylistList() {
+  var playslistList = document.getElementById("playslistList");
+  var playslistRef = new Firebase(FIREBASE_REF).child("playlists");
+  playslistRef.on("value", function(snapshot) {
+    playslistList.innerHTML = "";
+      snapshot.forEach(function(data) {
+          var id = data.key();
+          var playlist = data.val();
+          var name = playlist.name;
+          var option = document.createElement('option');
+          option.id = option.value = id;
+          option.text =  name;
+          playslistList.appendChild(option);
+      });
+  });
+}
+
+function playPlaylist() {
+  var playslistList = document.getElementById("playslistList");
+  var selectedPlaylist = playslistList.value;
+  var controls = new Firebase(FIREBASE_REF).child("controls/command");
+  var command = createCommand(true, "setGenre", {"genre" : selectedPlaylist});
+  controls.push(command);
+}
